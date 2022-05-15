32bfc75cf67e0d6cc2825bd88997a9a755585c91
Shriprasanna B <shriprasanna32@gmail.com>
2022-05-15 19:15:10 +0530

    commets added

--- paint.js
+++ paint.js
@@ -1,79 +1,87 @@
 //var
 const allColor = document.querySelectorAll(".color");
-const clear = document.querySelector("#eraser").addEventListener("click",eraser);
-const clearFullCanvas = document.querySelector("#trash").addEventListener("click", clearCanvas);
+const clear = document.querySelector("#eraser");
+const clearFullCanvas = document.querySelector("#trash");
+
+//var to implement in change of color ,sizeOfPen and others
 let color = "lightBlue";
 let penSize = 5;
 let painting = false;
-let bgColor = 'white';
+let bgColor = "white";
 
 //canvas.js
 window.addEventListener("load", () => {
   const canvas = document.querySelector("#canvas");
   const context = canvas.getContext("2d");
 
-  function start(e){
-    painting = true;
+  function start(e) {
+    painting = true; //f
     draw(e);
   }
 
-  function finish(){
-    painting = false;
+  function finish() {
+    painting = false; //t
     context.beginPath();
   }
 
-  function draw(e){
+  function draw(e) {
     if (!painting) return;
+    // if return false below code will run
 
     context.strokeStyle = color; //var color
     context.lineWidth = penSize; //var size
-    context.lineCap = "round";
+    context.lineCap = "round"; // rounded edges
 
+    //getting xand y value
     let x = e.offsetX;
     let y = e.offsetY;
 
-    context.lineTo(x, y);
-    context.stroke();
-    context.beginPath();
-    context.moveTo(x, y);
+    context.lineTo(x, y); //  -----------
+    context.stroke(); // ---- == ===
+    context.beginPath(); // .--.
+    context.moveTo(x, y); // -----/----\--
   }
 
-  canvas.addEventListener("mousedown", start);
-  canvas.addEventListener("mouseup", finish);
-  canvas.addEventListener("mousemove", draw);
+  canvas.addEventListener("mousedown", start); //holded mouse to draw
+  canvas.addEventListener("mouseup", finish); //release mouse
+  canvas.addEventListener("mousemove", draw); //based on start,end function return value
 });
 
+//eraser and fullCanvas clear listeners
+clear.addEventListener("click", eraser);
+clearFullCanvas.addEventListener("click", clearCanvas);
+
 //active button and get colors
-function selectColor(e){
+function selectColor(e) {
   removeActive();
   color = e.getAttribute("data-value");
   e.classList.add("active");
 }
-function removeActive(){
+function removeActive() {
   allColor.forEach((c) => {
     c.classList.remove("active");
   });
 }
 
-//sizeof the pen
-function penSizeChanging(size){
+//sizeOf the pen
+function penSizeChanging(size) {
   penSize = size;
 }
 
 //pick color
-function pickedColorChange(col){
+function pickedColorChange(col) {
   color = col.value;
 }
 
 // eraser
-function eraser(){
-  penSize = 10
+function eraser() {
+  penSize = 10;
   color = "black";
 }
 
 //clearCanvas
-function clearCanvas(){
-  location.reload()  //bad idea
+function clearCanvas() {
+  location.reload(); //bad idea
 }
 
 //download your art
